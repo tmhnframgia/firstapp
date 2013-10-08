83a07c942e5e88b4b6d139e7e5aaea62654ab2eb
Truong My Hai Ninh <truong.my.hai.ninh@framgia.com>
2013-10-08 10:48:49 +0700

    Superman first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstapp::Application.config.secret_key_base = 'daefc6c54ad6ec7954435bdb1359f6c9f7cd0530317b78334386d14631c4a9eba51f6505744970bf55a2a12ea6dad055641419ad2d0bd48bae608bca6838e79a'
